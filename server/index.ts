e0a3691b6b6504a2713b61e7d42052340c9d140d
Thomas Bonk <tbo@cybo.biz>
2017-11-11 17:33:56 +0100

    Set NODE_ENV and clean up

--- server/index.ts
+++ server/index.ts
@@ -2,13 +2,16 @@ import * as express from 'express';
 import * as webpackMiddleware from 'webpack-dev-middleware';
 import * as webpack from 'webpack';
 
-const webpackMiddlewareConfig = {
-  stats: 'errors-only',
-	publicPath: '/'
-}
-const webpackCompiler = webpack(require('../webpack.config'));
 const app = express();
 
-app.use(webpackMiddleware(webpackCompiler, webpackMiddlewareConfig));
+if (process.env.NODE_ENV === 'development')  {
+  const webpackMiddlewareConfig = {
+    stats: 'errors-only',
+    publicPath: '/'
+  }
+  const webpackCompiler = webpack(require('../webpack.config'));
+  app.use(webpackMiddleware(webpackCompiler, webpackMiddlewareConfig));
+}
+
 app.use('/', express.static('public'));
 app.listen(3000);
