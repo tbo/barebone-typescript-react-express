3439f5ddf32b7632979b2bb15ef76a3a5be8fd65
Thomas Bonk <tbo@cybo.biz>
2017-11-12 17:55:51 +0100

    Added query support

--- client/router.ts
+++ client/router.ts
@@ -1,6 +1,7 @@
 import createHistory from 'history/createBrowserHistory'
 import Homepage from './pages/home';
 import NotFound from './pages/not-found';
+import {parse} from 'qs';
 import * as UrlPattern from 'url-pattern';
 
 interface IRouteDefinition {
@@ -11,6 +12,7 @@ interface IRouteDefinition {
 
 export interface IRoute extends IRouteDefinition {
   params: object;
+  query: object;
 }
 
 const addPattern = item => ({...item, pattern: new UrlPattern(item.path)});
@@ -23,20 +25,22 @@ const routes: IRouteDefinition[] = [
 
 const listeners = [];
 
-export const listen = (listener: Function) => listeners.push(listener);
+const history = createHistory();
 
 const getRoute = (location): IRoute => {
   let params = {};
-  return {...routes.find(({pattern}) => params = pattern.match(location.pathname)), params};
+  const query = parse(location.search.substring(1));
+  return {...routes.find(({pattern}) => params = pattern.match(location.pathname)), params, query};
 };
-
-const history = createHistory();
-
-export const getCurrentRoute = () => getRoute(history.location);
 
 history.listen((location) => {
   const route = getRoute(location);
+  console.warn(route);
   listeners.forEach(listener => listener(route));
 })
+
+export const getCurrentRoute = () => getRoute(history.location);
+
+export const listen = (listener: Function) => listeners.push(listener);
 
 export const navigateTo = (to: string) => history.push(to);
